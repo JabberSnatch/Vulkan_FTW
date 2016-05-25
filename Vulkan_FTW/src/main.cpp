99a4b4912b7a898f14a973702d0caa09b95f0b38
JabberSnatch <samuel.bourasseau@gmail.com>
2016-05-25 16:26:48 +0200

    vk_init should be complete. Next step is to create the swapchain.

--- Vulkan_FTW/src/main.cpp
+++ Vulkan_FTW/src/main.cpp
@@ -5,14 +5,40 @@
 
 #include <iostream>
 
+#include <cstdlib>
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+
+#define APP_NAME "Vulkan FTW"
+#define ENGINE_NAME "YOLORenderSQUAD"
+
+
 static HINSTANCE	win_instance;
 static LPCSTR		win_class_name = "vulkan_render_window";
-static LPCSTR		win_app_name = "Vulkan FTW";
+static LPCSTR		win_app_name = APP_NAME;
 static LONG			win_width = 800;
 static LONG			win_height = 640;
 static HWND			window_handle;
 
 static bool			vk_validate = true; // Tells the application if it should load Vulkan's validation layers.
+static char*		vk_instance_layers[] = {
+	"VK_LAYER_LUNARG_standard_validation"
+};
+static char*		vk_instance_extensions[] = {
+	"VK_KHR_surface",
+	"VK_KHR_win32_surface",
+	"VK_EXT_debug_report"
+};
+static char*		vk_device_extensions[] = {
+	"VK_KHR_swapchain"
+};
+
+static VkInstance				vk_instance;
+
+static VkPhysicalDevice			vk_gpu;
+static uint32_t					vk_queue_count = 0;
+static VkQueueFamilyProperties*	vk_queue_props = nullptr;
+
 
 
 static void create_window();
@@ -21,6 +47,7 @@ int main(int, char**);
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int);
 
 static void vk_init();
+static void vk_shutdown();
 
 
 static void
@@ -115,6 +142,8 @@ WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int)
 		RedrawWindow(window_handle, NULL, NULL, RDW_INTERNALPAINT);
 	}
 
+	vk_shutdown();
+
 	return (int)msg.wParam;
 }
 
@@ -123,10 +152,15 @@ static void
 vk_init()
 {
 	VkResult	error;
-	uint32_t	instance_layer_count = 0;
 
+	// NOTE: If any requested layer or extension is not supported by the driver,
+	//		 an assert is raised. 
+
+	// VALIDATION LAYERS CHECK
 	if (vk_validate)
 	{
+		uint32_t	instance_layer_count = 0;
+
 		error = vkEnumerateInstanceLayerProperties(&instance_layer_count, nullptr);
 		assert(!error);
 
@@ -137,14 +171,158 @@ vk_init()
 			error = vkEnumerateInstanceLayerProperties(&instance_layer_count, instance_layers);
 			assert(!error);
 
-			for (uint32_t i = 0; i < instance_layer_count; ++i)
+			// Make sure that every validation layer that we want is supported by the ICD.
+			bool validation_ok = true;
 			{
-				VkLayerProperties& layer_prop = instance_layers[i];
-				std::cout << layer_prop.layerName << " : " << layer_prop.description << std::endl;
+				for (uint32_t expected = 0; expected < ARRAY_SIZE(vk_instance_layers) && validation_ok; ++expected) 
+				{
+					char*	expected_layer = vk_instance_layers[expected];
+					bool	expected_ok = false;
+					for (uint32_t available = 0; available < instance_layer_count && !expected_ok; ++available)
+					{
+						expected_ok = !strcmp(expected_layer, instance_layers[available].layerName);
+					}
+					validation_ok &= expected_ok;
+				}
 			}
+			assert(validation_ok);
 
 			delete[] instance_layers;
 		}
 	}
+
+	// EXTENSIONS CHECK
+	{
+		uint32_t	instance_extension_count = 0;
+
+		error = vkEnumerateInstanceExtensionProperties(nullptr, &instance_extension_count, nullptr);
+		assert(!error);
+
+		if (instance_extension_count > 0)
+		{
+			VkExtensionProperties* instance_extensions = new VkExtensionProperties[instance_extension_count];
+
+			error = vkEnumerateInstanceExtensionProperties(nullptr, &instance_extension_count, instance_extensions);
+			assert(!error);
+
+			bool extensions_ok = true;
+			{
+				for (uint32_t expected = 0; expected < ARRAY_SIZE(vk_instance_extensions) && extensions_ok; ++expected)
+				{
+					char*	expected_extension = vk_instance_extensions[expected];
+					bool	expected_ok = false;
+					for (uint32_t available = 0; available < instance_extension_count && !expected_ok; ++available)
+					{
+						expected_ok = !strcmp(expected_extension, instance_extensions[available].extensionName);
+					}
+					extensions_ok &= expected_ok;
+				}
+			}
+			assert(extensions_ok);
+
+			delete[] instance_extensions;
+		}
+	}
+
+	// INSTANCE
+	{
+		VkInstanceCreateInfo	instance_info;
+		VkApplicationInfo		app_info;
+
+		// VkApplicationInfo
+		{
+			app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+			app_info.pNext = nullptr;
+			app_info.pApplicationName = APP_NAME;
+			app_info.applicationVersion = 1;
+			app_info.pEngineName = ENGINE_NAME;
+			app_info.engineVersion = 1;
+			app_info.apiVersion = VK_API_VERSION_1_0;
+		}
+
+		// VkInstanceCreateInfo
+		{
+			instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+			instance_info.pNext = nullptr;
+			instance_info.pApplicationInfo = &app_info;
+			instance_info.enabledLayerCount = (uint32_t)ARRAY_SIZE(vk_instance_layers);
+			instance_info.ppEnabledLayerNames = vk_instance_layers;
+			instance_info.enabledExtensionCount = (uint32_t)ARRAY_SIZE(vk_instance_extensions);
+			instance_info.ppEnabledExtensionNames = vk_instance_extensions;
+		}
+
+		error = vkCreateInstance(&instance_info, nullptr, &vk_instance);
+		assert(!error);
+	}
+
+	// PHYSICAL DEVICE
+	{
+		uint32_t	gpu_count = 0;
+
+		error = vkEnumeratePhysicalDevices(vk_instance, &gpu_count, nullptr);
+		assert(!error && gpu_count);
+
+		VkPhysicalDevice* instance_physical_devices = new VkPhysicalDevice[gpu_count];
+		error = vkEnumeratePhysicalDevices(vk_instance, &gpu_count, instance_physical_devices);
+		assert(!error);
+
+		vk_gpu = instance_physical_devices[0];
+		delete[] instance_physical_devices;
+	}
+	// NOTE: We assume that the application runs on a single GPU computer. Otherwise, we would 
+	//		 have to check our desired layers against the selected GPU available layers.
+
+	// LOOK FOR DEVICE EXPECTED EXTENSION
+	{
+		uint32_t	device_extension_count = 0;
+
+		error = vkEnumerateDeviceExtensionProperties(vk_gpu, nullptr, &device_extension_count, nullptr);
+		assert(!error && device_extension_count);
+
+		VkExtensionProperties* device_extensions = new VkExtensionProperties[device_extension_count];
+		error = vkEnumerateDeviceExtensionProperties(vk_gpu, nullptr, &device_extension_count, device_extensions);
+		assert(!error);
+
+		bool extensions_ok = true;
+		{
+			for (uint32_t expected = 0; expected < ARRAY_SIZE(vk_device_extensions) && extensions_ok; ++expected)
+			{
+				char*	expected_extension = vk_device_extensions[expected];
+				bool	expected_ok = false;
+				for (uint32_t available = 0; available < device_extension_count && !expected_ok; ++available)
+				{
+					expected_ok = !strcmp(expected_extension, device_extensions[available].extensionName);
+				}
+				extensions_ok &= expected_ok;
+			}
+		}
+		assert(extensions_ok);
+
+		delete[] device_extensions;																	
+	}
+
+	// SELECT A QUEUE FROM THE PHYSICAL DEVICE
+	{
+		vkGetPhysicalDeviceQueueFamilyProperties(vk_gpu, &vk_queue_count, nullptr);
+		assert(vk_queue_count > 0);
+
+		vk_queue_props = new VkQueueFamilyProperties[vk_queue_count];
+		vkGetPhysicalDeviceQueueFamilyProperties(vk_gpu, &vk_queue_count, vk_queue_props);
+
+		uint32_t	elected_queue_index = 0;
+		for (elected_queue_index = 0; elected_queue_index < vk_queue_count; elected_queue_index++)
+		{
+			if (vk_queue_props[elected_queue_index].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+				break;
+		}
+		assert(elected_queue_index < vk_queue_count);
+	}
+}
+
+
+static void
+vk_shutdown()
+{
+	delete[] vk_queue_props;
 }
 
